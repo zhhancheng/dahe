26539c1c95df31d56408548b7deecc023ae7e612
soft01 <soft01@server-176.53.6.104.as42926.net>
2018-09-16 16:34:03 +0800

    提交

---
+++ src/day03/AgeRange.java
@@ -0,0 +1,103 @@
+package day03;
+//import java.util.Scanner;
+public class AgeRange {
+
+	public static void main(String[] args) {
+		//(第一题)
+		int a = 5;
+		a += 10;
+		System.out.println(a);//15
+		a /= 5;
+		System.out.println(a);//3
+		a *= 8;
+		System.out.println(a);//24
+		//(第二题)
+		int age = 37;
+		System.out.println("age="+age);//age = 37
+		System.out.println("我的年龄是："+age);//我的年龄是37
+		System.out.println("我今年"+age+"岁了");//我今年37岁了
+		System.out.println(10+""+20);//1020
+		System.out.println(10+20+"");//30
+		System.out.println(""+10+20);//1020
+		//(第三题)
+		int b = 8,c = 55;
+		int  max =  b>c?b:(b>c?b:c);
+		System.out.println(max);//8
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/*
+		//Scanner scan = new Scanner(System.in);
+		int a = 5,b = 10,c = 5;
+		System.out.println(a<=b && b<c);//flass
+		System.out.println(b==c && a<=c);//flass
+		System.out.println(a>b && b>c);//flass
+		System.out.println(a==c && b>a);//true
+		System.out.println(a<b || b>c);//true
+		System.out.println(b>a || a<c);//true
+		System.out.println(c==a || b<c);//true
+		System.out.println(a>b || b<c);//flass
+		System.out.println(!(a<b));//flass
+		System.out.println(!(b<c));//true
+		System.out.println(a<b && b<c++);//flass 5
+		System.out.println(c);//6
+		System.out.println(a>b || b>a++);//true 6
+		System.out.println(c);//6
+		
+		
+		
+		
+		*/
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/*
+		System.out.println("请输入年龄");
+		int age = scan.nextInt();
+		boolean year = age>=18&&age>=50;
+		System.out.println(year);
+		*/
+	}
+
+}
